2e6f7d85cde856cd6414181ce2a2226704b86dfc
Yaqub <48588063+quber0161@users.noreply.github.com>
2025-03-27 20:29:40 +0100

    database stripe

--- backend-restaurant/config/db.js
+++ backend-restaurant/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dilushan06:0606@cluster0.xbo2f.mongodb.net/restaurant').then(() => console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://SHANDIZ:SHANDIZ123@cluster0.1hehu.mongodb.net/SHANDIZ').then(() => console.log("DB Connected"));
 }
 
