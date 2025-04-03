df839c7ed12ef3cc0282b5bb5dbd594b62629e1e
Yaqub <48588063+quber0161@users.noreply.github.com>
2025-04-03 20:51:37 +0200

    stripe-betalin

--- backend-restaurant/controllers/orderController.js
+++ backend-restaurant/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order from front end
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://restaurant-website-52z8l4wz3-yaqubs-projects-095d7162.vercel.app";
 
     try {
         const userId = req.userId;
