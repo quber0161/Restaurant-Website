b18f58e8bb55d81a7ecdb4ee6709cab8e31c2b64
Yaqub <48588063+quber0161@users.noreply.github.com>
2025-04-14 20:09:12 +0200

    fix: added missing exports in payment

--- frontend-restaurant/src/NotFound.jsx
+++ frontend-restaurant/src/NotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>404 - Page Not Found</h1>
+      <h1>søppel error</h1>
       <p>The page you are looking for does not exist.</p>
       <Link to="/">Go Home</Link>
     </div>
